94108b10283b5985d8bd09dac844511f1af42b77
beg beg <sweet.bebe67@gmail.com>
2022-06-13 23:12:07 +0300

    pagination component

--- src/components/new-table.tsx
+++ src/components/new-table.tsx
@@ -12,6 +12,7 @@ import {
     Box,
 } from "@chakra-ui/react";
 import GlobalFilter from "./globalFilter"
+import Pagination from './pagination';
 
 
 
@@ -133,58 +134,23 @@ function EventTable({ columns, data, updateMyData, skipPageReset }: Ctype) {
                             </Tr>
                         )
                     })}
+
                 </Tbody>
             </Table>
             <div className="pagination">
-                <Flex>
-                    <Box flex='1'>
-                        <Button onClick={() => gotoPage(0)} disabled={!canPreviousPage}>
-                            {'<<'}
-                        </Button>{' '}
-                        <Button onClick={() => previousPage()} disabled={!canPreviousPage}>
-                            {'<'}
-                        </Button>{' '}
-                        <Button onClick={() => nextPage()} disabled={!canNextPage}>
-                            {'>'}
-                        </Button>{' '}
-                        <Button onClick={() => gotoPage(pageCount - 1)} disabled={!canNextPage}>
-                            {'>>'}
-                        </Button>{' '}
-                        <span>
-                            Page{' '}
-                            <strong>
-                                {pageIndex + 1} of {pageOptions.length}
-                            </strong>{' '}
-                        </span>
-                        <span>
-                            | Go to page:{' '}
-                            <input
-                                type="number"
-                                defaultValue={pageIndex + 1}
-                                onChange={e => {
-                                    const page = e.target.value ? Number(e.target.value) - 1 : 0
-                                    gotoPage(page)
-                                }}
-                                style={{ width: '100px' }}
-                            />
-                        </span>{' '}
-                        <select
-                            value={pageSize}
-                            onChange={e => {
-                                setPageSize(Number(e.target.value))
-                            }}
-                        >
-                            {[10, 20, 30, 40, 50].map(pageSize => (
-                                <option key={pageSize} value={pageSize}>
-                                    Show {pageSize}
-                                </option>
-                            ))}
-                        </select>
-                    </Box>
-
-                </Flex>
-
-
+                <Pagination
+                    nextPage={nextPage}
+                    setPageSize={setPageSize}
+                    page={page}
+                    pageIndex={pageIndex}
+                    gotoPage={gotoPage}
+                    canPreviousPage={canPreviousPage}
+                    previousPage={previousPage}
+                    canNextPage={canNextPage}
+                    pageCount={pageCount}
+                    pageOptions={pageOptions}
+                    pageSize={pageSize}
+                ></Pagination>
             </div>
 
 
---
+++ src/components/pagination.tsx
@@ -0,0 +1,77 @@
+import {
+    Button,
+    Flex,
+    Box,
+} from "@chakra-ui/react";
+
+
+export default function Pagination({
+    page,
+    canPreviousPage,
+    canNextPage,
+    pageOptions,
+    pageCount,
+    gotoPage,
+    nextPage,
+    previousPage,
+    setPageSize, pageIndex, pageSize
+
+
+}: any
+) {
+
+    return (
+
+
+        <Flex>
+            <Box flex='1'>
+                <Button onClick={() => gotoPage(0)} disabled={!canPreviousPage}>
+                    {'<<'}
+                </Button>{' '}
+                <Button onClick={() => previousPage()} disabled={!canPreviousPage}>
+                    {'<'}
+                </Button>{' '}
+                <Button onClick={() => nextPage()} disabled={!canNextPage}>
+                    {'>'}
+                </Button>{' '}
+                <Button onClick={() => gotoPage(pageCount - 1)} disabled={!canNextPage}>
+                    {'>>'}
+                </Button>{' '}
+                <span>
+                    Page{' '}
+                    <strong>
+                        {pageIndex + 1} of {pageOptions.length}
+                    </strong>{' '}
+                </span>
+                <span>
+                    | Go to page:{' '}
+                    <input
+                        type="number"
+                        defaultValue={pageIndex + 1}
+                        onChange={e => {
+                            const page = e.target.value ? Number(e.target.value) - 1 : 0
+                            gotoPage(page)
+                        }}
+                        style={{ width: '100px' }}
+                    />
+                </span>{' '}
+                <select
+                    value={pageSize}
+                    onChange={e => {
+                        setPageSize(Number(e.target.value))
+                    }}
+                >
+                    {[10, 20, 30, 40, 50].map(pageSize => (
+                        <option key={pageSize} value={pageSize}>
+                            Show {pageSize}
+                        </option>
+                    ))}
+                </select>
+            </Box>
+
+        </Flex>
+
+
+    );
+}
+
