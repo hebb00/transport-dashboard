b21f1609ab37c8477a0dd4b4db13ac4e8d6aaf22
beg beg <sweet.bebe67@gmail.com>
2022-06-22 20:04:18 +0300

    adding charts

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './styles/index.css'
 import App from './routes/App'
 import {
   BrowserRouter as Router,
@@ -9,27 +8,57 @@ import {
 } from "react-router-dom";
 import Navbar from "./components/navbar";
 import Vehicle from "./routes/vehicle";
-import Jobs from "./routes/jobs";
+import Clients from "./routes/clients";
 import Driver from "./routes/driver";
 import Login from "./routes/login";
-import { ChakraProvider } from '@chakra-ui/react';
+import { Dashboard } from "./routes/dashboard"
 
-
+import { ChakraProvider, Flex, Box } from '@chakra-ui/react';
+import Sidebar from './components/sidebar'
+import { Text, IconButton } from '@chakra-ui/react'
+import { FiMenu } from 'react-icons/fi'
 
 ReactDOM.render(
   <React.StrictMode>
+
+
     <Router>
       <ChakraProvider>
-        <Navbar />
+        <Flex w="100%"
+          flexDirection="row"
+        >
+          <Box
+          >
+            <Sidebar />
 
-        <Routes>
-          <Route path="/" element={<App />} />
-          <Route path="/drivers" element={<Driver />} />
-          <Route path="/vehicle" element={<Vehicle />} />
-          <Route path="/jobs" element={<Jobs />} />
-          <Route path="/login" element={<Login />} />
+          </Box>
+          <Box
+            flex={1}
+          >
 
-        </Routes>
+            <Navbar />
+
+
+
+            <Routes>
+              <Route path="/" element={<App />} />
+              <Route path="/drivers" element={<Driver />} />
+              <Route path="/vehicle" element={<Vehicle />} />
+              <Route path="/clients" element={<Clients />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/dashboard" element={< Dashboard />} />
+
+            </Routes>
+
+
+
+
+          </Box>
+
+
+        </Flex>
+
+
       </ChakraProvider>
 
     </Router>
---
+++ src/routes/dashboard.tsx
@@ -0,0 +1,163 @@
+import React from 'react';
+import {
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    Title,
+    Tooltip,
+    Legend,
+} from 'chart.js';
+import { Bar } from 'react-chartjs-2';
+import faker from 'faker';
+import { Box, Flex } from '@chakra-ui/react';
+
+import { ArcElement } from 'chart.js';
+import { Pie } from 'react-chartjs-2';
+
+import {
+
+    PointElement,
+    LineElement,
+
+} from 'chart.js';
+import { Line } from 'react-chartjs-2';
+
+
+ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend
+);
+
+export const opt = {
+    responsive: true,
+    plugins: {
+        legend: {
+            position: 'top' as const,
+        },
+        title: {
+            display: true,
+            text: 'Chart.js Line Chart',
+        },
+    },
+};
+
+const label = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+
+
+
+
+ChartJS.register(ArcElement, Tooltip, Legend);
+
+export const d = {
+    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+    datasets: [
+        {
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)',
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)',
+            ],
+            borderWidth: 1,
+        },
+    ],
+};
+
+
+ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    Title,
+    Tooltip,
+    Legend
+);
+
+export const options = {
+    responsive: true,
+    plugins: {
+        legend: {
+            position: 'top' as const,
+        },
+        title: {
+            display: true,
+            text: 'Chart.js Bar Chart',
+        },
+    },
+};
+
+const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+
+export const data = {
+    labels,
+    datasets: [
+        {
+            label: 'Dataset 1',
+            data: labels.map(() => faker.datatype.number({ min: 0, max: 1000 })),
+            backgroundColor: 'rgba(255, 99, 132, 0.5)',
+        },
+        {
+            label: 'Dataset 2',
+            data: labels.map(() => faker.datatype.number({ min: 0, max: 1000 })),
+            backgroundColor: 'rgba(53, 162, 235, 0.5)',
+        },
+    ],
+};
+
+
+export const dd = {
+    labels,
+    datasets: [
+        {
+            label: 'Dataset 1',
+            data: labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
+            borderColor: 'rgb(255, 99, 132)',
+            backgroundColor: 'rgba(255, 99, 132, 0.5)',
+        },
+        {
+            label: 'Dataset 2',
+            data: labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
+            borderColor: 'rgb(53, 162, 235)',
+            backgroundColor: 'rgba(53, 162, 235, 0.5)',
+        },
+    ],
+};
+
+
+export function Dashboard() {
+    return (
+
+        <Flex flexDirection="row">
+            <Box width={["40%", '50%', "80%"]}>
+                <Bar options={options} data={data} />
+
+            </Box>
+            <Box width={['20%', '40%', '50%']}>
+                <Pie data={d} />
+            </Box>
+            <Box width={["40%", '50%', "80%"]}>
+                <Line options={opt} data={dd} />
+            </Box>
+        </Flex>
+
+    )
+
+}
