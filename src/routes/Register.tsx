e2ce4cd0914bbdeb4ae79100729308bbaf56589a
beg beg <sweet.bebe67@gmail.com>
2022-07-02 14:04:35 +0300

    rename signup

---
+++ src/routes/Register.tsx
@@ -0,0 +1,120 @@
+import { ChakraProvider, ColorModeProvider } from "@chakra-ui/react"
+//Optionally, you can wrap your application with
+// the ColorModeProvider so you can toggle between light and dark mode
+import { useState } from "react";
+
+import { useNavigate } from "react-router-dom";
+import {
+    Flex,
+    Heading,
+    Input,
+    Button,
+    InputGroup,
+    Stack,
+    InputLeftElement,
+    chakra,
+    Box,
+    Link,
+    Avatar,
+    FormControl,
+    FormHelperText,
+    InputRightElement
+} from "@chakra-ui/react";
+import { FaUserAlt, FaLock } from "react-icons/fa";
+const CFaUserAlt = chakra(FaUserAlt);
+const CFaLock = chakra(FaLock);
+
+function Register() {
+
+    const [showPassword, setShowPassword] = useState(false);
+
+    const handleShowClick = () => setShowPassword(!showPassword);
+
+    let navigate = useNavigate();
+
+    // function handleClick() {
+    //     navigate("../login");
+
+    // }
+    return (
+        <Flex
+            flexDirection="column"
+            width="100wh"
+            height="100vh"
+            bg='gray.50'
+            justifyContent="center"
+            alignItems="center"
+        >
+            <Stack
+                flexDir="column"
+                mb="2"
+                justifyContent="center"
+                alignItems="center"
+            >
+                <Avatar bg="teal.500" />
+                <Heading color="teal.400">Welcome</Heading>
+                <Box minW={{ base: "90%", md: "468px" }}>
+                    <form >
+                        <Stack
+                            spacing={4}
+                            p="1rem"
+                            backgroundColor="whiteAlpha.900"
+                            boxShadow="md"
+                        >
+                            <FormControl>
+                                <InputGroup>
+                                    <InputLeftElement
+                                        pointerEvents="none"
+                                        children={<CFaUserAlt color="gray.300" />}
+                                    />
+                                    <Input type="email" placeholder="email address" />
+                                </InputGroup>
+                            </FormControl>
+                            <FormControl>
+                                <InputGroup>
+                                    <InputLeftElement
+                                        pointerEvents="none"
+                                        color="gray.300"
+                                        children={<CFaLock color="gray.300" />}
+                                    />
+                                    <Input
+                                        type={showPassword ? "text" : "password"}
+                                        placeholder="Password"
+                                    />
+                                    <InputRightElement width="4.5rem">
+                                        <Button h="1.75rem" size="sm" onClick={handleShowClick}>
+                                            {showPassword ? "Hide" : "Show"}
+                                        </Button>
+                                    </InputRightElement>
+                                </InputGroup>
+                                <FormHelperText textAlign="right">
+                                    <Link>forgot password?</Link>
+                                </FormHelperText>
+                            </FormControl>
+                            <Button
+                                borderRadius={0}
+                                type="submit"
+                                variant="solid"
+                                colorScheme="teal"
+                                width="full"
+                            >
+                                Login
+                            </Button>
+                        </Stack>
+                    </form>
+                </Box>
+            </Stack>
+            <Box>
+                New to us?{" "}
+                <Link color="teal.500" href="#">
+                    Sign Up
+        </Link>
+            </Box>
+        </Flex>
+
+
+
+    )
+}
+
+export default Register
