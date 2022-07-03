228ca613a401e90e27aaf6efde73996d40db53f3
beg beg <sweet.bebe67@gmail.com>
2022-07-03 20:45:15 +0300

    fetching data from the backend

--- src/routes/App.tsx
+++ src/routes/App.tsx
@@ -1,12 +1,46 @@
-import Schedular from "../components/schedular"
-import { ChakraProvider, ColorModeProvider } from "@chakra-ui/react"
+import Schedular from "../components/reservations/schedular"
+import { Button, ChakraProvider, ColorModeProvider } from "@chakra-ui/react"
+import { useState, useEffect } from "react";
 //Optionally, you can wrap your application with
 // the ColorModeProvider so you can toggle between light and dark mode
 function App() {
+
+
+  const [res, setRes] = useState("");
+
+
+  useEffect(() => {
+
+    fetch("http://localhost:9000/").then(
+      Response => Response.text()
+    ).then(
+      Response => setRes(Response)
+
+    )
+    console.log("res", res)
+
+
+  }, [])
+
+
+
+
   return (
     <ChakraProvider>
       <div className="App">
         <Schedular></Schedular>
+        {
+          (typeof res == "undefined") ? (
+            <p>loading..</p>
+          ) : (
+              // res.users.map((user: any, i: any) => {
+              <p >{res}</p>
+
+              // })
+            )
+        }
+
+
       </div>
 
     </ChakraProvider>
