702158de0da27d4a5465673f064c1261a8e04b03
beg beg <sweet.bebe67@gmail.com>
2022-07-10 17:37:17 +0300

    upgrading packages and addig license key

--- src/main.tsx
+++ src/main.tsx
@@ -20,7 +20,13 @@ import { FiMenu } from 'react-icons/fi'
 import Profile from './routes/profile';
 import Reservation from './routes/reservation';
 import Register from './routes/Register';
+import * as b from '@syncfusion/ej2-base';
+// Registering Syncfusion license key
+import { registerLicense } from '@syncfusion/ej2-base';
 
+registerLicense('ORg4AjUWIQA/Gnt2VVhiQlFadVlJXGFWfVJpTGpQdk5xdV9DaVZUTWY/P1ZhSXxRdkJjXn5ecXFRR2JYU0Q=');
+
+// registerLicense('@32302e322e30BVRYOcJJO9E+cuJe3VFpmU6PAfzAXbyX0fFQkIkB7Sg=');
 ReactDOM.render(
   <React.StrictMode>
 
